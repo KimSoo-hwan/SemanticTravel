8cbe1941cef3aa68ce3ea2b41d291abb97559e1f
Park jinsu <bluesky32132489@gmail.com>
2019-10-12 04:11:21 +0900

    10.12
    기울기,점프,소리

--- New Unity Project/Assets/Scripts/PlayerMove.cs
+++ New Unity Project/Assets/Scripts/PlayerMove.cs
@@ -7,48 +7,71 @@ using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-    public float jumpSpeed = 300.0f;
+    public float moveSpeed = 0.07f;
+    public float moveSpeedKeyboard = 5f;
+    public float dirX;
+    public float jumpSpeed = 250f;
+
     public bool isGorunded = false; //바닥체크
     public bool gameStart = false;
+
+
     Rigidbody2D rb;
     public int jumpCount = 2;   //2단점프
     public Text touchtoStart;
 
-    
-    
+
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         jumpCount = 0;
         gameStart = false;
-        
+
     }
 
-    //바갇 충돌체크함수
-    private void OnCollisionEnter2D(Collision2D collision) {
+    void Update()
+    {
+    
+        GameStat();
+        PlayerJump();
+        Player_Move();
+
+    }
+
+    //바닥 충돌체크함수 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         if (collision.gameObject.tag == "Ground")
         {
             isGorunded = true;
             jumpCount = 2;
+            GetComponent<AudioSource>().Play();
             Debug.Log("Ground col");
         }
     }
 
-
-    void Update()
+    //최초시작버튼
+    void GameStat()
     {
-        if (gameStart == false && Input.GetMouseButtonDown(0)) {
+        if (gameStart == false && Input.GetMouseButtonDown(0))
+        {
             gameStart = true;
             Destroy(touchtoStart);
 
         }
+    }
+
+    //점프
+    void PlayerJump()
+    {
         if (gameStart == true)
         {
             rb.constraints = RigidbodyConstraints2D.None;
             rb.constraints = RigidbodyConstraints2D.FreezeRotation;
 
-            //점프함수
+            
             if (jumpCount > 0)
             {
                 if (isGorunded == true)
@@ -66,23 +89,42 @@ public class PlayerMove : MonoBehaviour
                 }
             }
 
-            //좌로이동, 기울기함수로 변경예정
+        }
+    }
+
+    void Player_Move()
+    {
+        //기울기 플레이어 조작함수, - 더 좋은 코드를 찾는중입니다.
+        //유니티리모트 안쓰고 컴퓨터에서 디버깅할때는 주석처리바람    
+        if (gameStart == true)
+        {
+            dirX = Input.acceleration.x * moveSpeed;
+            transform.Translate(dirX, 0, 0);
+
+
+            //좌로이동,키보드 디버깅용
             if (Input.GetKey(KeyCode.LeftArrow))
             {
                 Vector2 scale = transform.localScale;
                 scale.x = -Mathf.Abs(scale.x);
                 transform.localScale = scale;
-                transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+                transform.Translate(Vector2.left * moveSpeedKeyboard * Time.deltaTime);
             }
-            //우로이동, 기울기함수로 변경예정
+
+            //우로이동,  키보드 디버깅용
             if (Input.GetKey(KeyCode.RightArrow))
             {
                 Vector2 scale = transform.localScale;
                 scale.x = -Mathf.Abs(scale.x);
                 transform.localScale = scale;
-                transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                transform.Translate(Vector2.right * moveSpeedKeyboard * Time.deltaTime);
 
             }
         }
     }
+
+
+
+
+
 }
