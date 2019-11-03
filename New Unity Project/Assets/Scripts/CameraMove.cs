9dcde351cc46b16f3bdd8b587bedc9cd2a5cffeb
Park jinsu <bluesky32132489@gmail.com>
2019-11-03 18:00:17 +0900

    카메라 추적
    카메라가 플레이어를 따라다니도록 변경

---
+++ New Unity Project/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject target; //유저
+    public float moveSpeed; //속도
+    private Vector3 targetPostion; //대상의 현재위치값
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.gameObject != null) {
+            targetPostion.Set(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+
+            this.transform.position = Vector3.Lerp(this.transform.position, targetPostion, moveSpeed * Time.deltaTime);
+
+        }
+        
+    }
+}
--- New Unity Project/Assets/Scripts/_Gm.cs
+++ New Unity Project/Assets/Scripts/_Gm.cs
@@ -11,21 +11,24 @@ public class _Gm : MonoBehaviour
     public GameObject gameOverText;
     public GameObject retryButton;
     public GameObject player;
-
+    
     // Start is called before the first frame update
     void Start()
     {
+        
         gameOverText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (gameovertrigger == true)
         {
             GameOver();
         }
     }
+
 
 
     public void KillPlayer()
