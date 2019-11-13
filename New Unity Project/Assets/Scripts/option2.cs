d78a199bff0282281ea69fba5c3a1964b13a08fd
kim-gyeong-hun <rudgns4541@gmail.com>
2019-11-13 17:03:39 +0900

    옵션버튼(수정)
    수정해서 올립니다.
    아직 미완성이라...죄송합니다ㅠㅠ

---
+++ New Unity Project/Assets/Scripts/option2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class option2 : MonoBehaviour
+{
+    public GameObject HowGmPanel;
+    bool Isbool = false;
+    public void OpenPanel()
+    {
+
+        if (Isbool == false)
+        {
+            Isbool = true;
+            HowGmPanel.SetActive(true);
+            Debug.Log("I CAN'T SEE");
+        }
+        else if (Isbool == true)
+        {
+
+            HowGmPanel.SetActive(false);
+            Isbool = false;
+            Debug.Log("Yabal");
+        }
+
+    }
+}
