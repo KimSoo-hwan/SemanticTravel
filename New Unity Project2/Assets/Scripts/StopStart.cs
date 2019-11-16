c7d9adb8659dd4c7eb15cb7c3adaffbf0eaa2813
kim-gyeong-hun <rudgns4541@gmail.com>
2019-11-16 09:08:31 +0900

    이미지삽입+배경음악삽입

--- New Unity Project2/Assets/Scripts/StopStart.cs
+++ New Unity Project2/Assets/Scripts/StopStart.cs
@@ -1,25 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StopStart : MonoBehaviour
 {
     public GameObject StopPanel;
     bool Isbool = false;
+    public Sprite Start;
+    public Sprite Stop;
+    Image myImageComponent;
 
     public void ActivePauseBt()
     {
+        myImageComponent = GetComponent<Image>();
         if (Isbool == false)
         {
             Time.timeScale = 0;
             Isbool = true;
             StopPanel.SetActive(true);
+            myImageComponent.sprite = Stop;
         }
         else if (Isbool == true)
         {
             StopPanel.SetActive(false);
             Isbool = false;
             Time.timeScale = 1.0f;
+            myImageComponent.sprite = Start;
         }
     }
 }
