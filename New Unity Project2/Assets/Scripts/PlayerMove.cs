bb393978b582c37838cbdc8d8416e8da4d7e241e
jinsu7500 <56360477+jinsu7500@users.noreply.github.com>
2019-11-26 20:50:20 +0900

    추락 버그픽스
    버그픽스

--- New Unity Project2/Assets/Scripts/PlayerMove.cs
+++ New Unity Project2/Assets/Scripts/PlayerMove.cs
@@ -12,21 +12,22 @@ public class PlayerMove : MonoBehaviour
     public float moveSpeedKeyboard = 5f;
     public float dirX;
     public float jumpSpeed = 250f;
-    
+
     public bool isGorunded = false; //바닥체크
     public bool gameStart = false;
 
     Rigidbody2D rb;
     public int jumpCount = 1;   //2단점프
     public Text touchtoStart;
-    public Vector3 pos, pos2,fallpos;
+    public Vector3 pos, pos2, fallpos;
 
     public Transform groundCheck;
     public LayerMask whatisGround;
     public float checkRadius;
-    public float fallposSlice = 0.25f;
 
-    
+    public float fallposSlice = 0.25f;
+    public float MAXARRAY = 10f;
+    public float MINARRAY = 3f;
 
 
     void Start()
@@ -34,29 +35,30 @@ public class PlayerMove : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         jumpCount = 1;
         gameStart = false;
-        
+
     }
 
     void Update()
     {
-    
+
         GameStat();
         PlayerJump();
         Player_Move();
         Falldown();
-                     
-        
+
+
 
     }
     private void FixedUpdate()
     {
         isGorunded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatisGround);
-        if (isGorunded == true) {
+        if (isGorunded == true)
+        {
             isGorunded = true;
             jumpCount = 1;
-      
+
         }
-        
+
     }
 
     //바닥 충돌체크함수 
@@ -64,9 +66,9 @@ public class PlayerMove : MonoBehaviour
     {
         if (collision.gameObject.tag == "Ground")
         {
-          
+
             GetComponent<AudioSource>().Play();
-           
+
         }
     }
 
@@ -94,25 +96,25 @@ public class PlayerMove : MonoBehaviour
                 jumpCount = 1;
             }
 
-                if (jumpCount > 0)            
-                
-                {
-                    if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.UpArrow))
-                    {
-                        Debug.Log("jump");
-                        //addforce이용 jumpSpeed = 250
-                        //Rigidbody2D rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
-                        //rigidbody2D.AddForce(Vector3.up * jumpSpeed);
+            if (jumpCount > 0)
 
-                        //velocity이용 jummpSpeed = 6
-                        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpSpeed);
-                        
-                        jumpCount--;
-                        if (jumpCount == 0)
-                            isGorunded = false;
-                    }
+            {
+                if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    Debug.Log("jump");
+                    //addforce이용 jumpSpeed = 250
+                    //Rigidbody2D rigidbody2D = this.gameObject.GetComponent<Rigidbody2D>();
+                    //rigidbody2D.AddForce(Vector3.up * jumpSpeed);
+
+                    //velocity이용 jummpSpeed = 6
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpSpeed);
+
+                    jumpCount--;
+                    if (jumpCount == 0)
+                        isGorunded = false;
                 }
-            
+            }
+
 
         }
     }
@@ -150,43 +152,69 @@ public class PlayerMove : MonoBehaviour
 
     void Falldown()
     {
+        Vector3 fallpos; //추락거리
+        Vector3 fallLimit; //높이별 추락제한변수
+
+
+        if (isGorunded == true)
+        {
+            pos = this.gameObject.transform.position;
+        }
+        else if (isGorunded == false)
+        {
+            pos2 = this.gameObject.transform.position;
+        }
+        fallpos.y = pos.y - pos2.y;
+
+
+        fallLimit.y = MAXARRAY - (pos.y * fallposSlice);
+
+
+        if (fallpos.y > fallLimit.y && fallpos.y > MINARRAY)
+        {
+            GameObject.Find("Main Camera").GetComponent<_Gm>().gameovertrigger = true;
+        }
+        Debug.Log(fallLimit.y);
+
+    }
+
+    /*
+    void Falldown()
+    {
         Vector3 logPos1;
         logPos1.y = Mathf.Log10(pos.y);
         fallpos.y = (pos.y * logPos1.y ) * fallposSlice;
 
         //fallpos.y = pos.y * Mathf.Log10(pos.y);
+
         //Debug.Log(fallpos.y *0.25);
         if (jumpCount == 1)
         {
             //현재높이
 
             pos = this.gameObject.transform.position;
-            //   Debug.Log(pos.y);
+             Debug.Log(fallpos.y);
             if (Mathf.Abs(pos2.y) - Mathf.Abs(pos.y) > fallpos.y && Mathf.Abs(pos2.y) - Mathf.Abs(pos.y) > 1)
-            {
+            {             
                 GameObject.Find("Main Camera").GetComponent<_Gm>().gameovertrigger = true;
             }
         }
-
-
-
         if (jumpCount < 1)
         {
             //가변높이
             pos2 = this.gameObject.transform.position;
-            // Debug.Log(pos2.y);
+            Debug.Log(fallpos.y);
             if (Mathf.Abs(pos.y) - Mathf.Abs(pos2.y) > fallpos.y && Mathf.Abs(pos.y) - Mathf.Abs(pos2.y) >1)
             {
                 GameObject.Find("Main Camera").GetComponent<_Gm>().gameovertrigger = true;
             }
         }
-
-
-
     }
+    */
 
 
-    
+
+
 
 
 
