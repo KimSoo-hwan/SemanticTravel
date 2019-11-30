9271ab00d475c4da74a90dd199dff73c95bad9f9
Park jinsu <bluesky32132489@gmail.com>
2019-11-30 22:55:52 +0900

    Countdown 함수추가
    일시정지 해제시 3초카운트다운후 게임재시작

--- New Unity Projec3/Assets/Scripts/StopStart.cs
+++ New Unity Projec3/Assets/Scripts/StopStart.cs
@@ -8,7 +8,7 @@ public class StopStart : MonoBehaviour
 {
     public float timeStart = 3;
     public Text textBox;
-    
+
 
     int del;
 
@@ -16,50 +16,59 @@ public class StopStart : MonoBehaviour
     bool Isbool = false;    //정지상태 유무
     public Sprite Start;    //버튼이미지
     public Sprite Stop;     //버튼이미지
-   
+
+
     Image myImageComponent;
 
     void start()
     {
-        textBox.text = timeStart.ToString();              
+        textBox.text = timeStart.ToString();
+
     }
 
     void Update()
     {
-        if (del == 1)
-        {           
-            timeStart -= Time.deltaTime;    //시간 내려가는거
-            textBox.text = Mathf.Round(timeStart).ToString();
-            Time.timeScale = 1.0f;
-            if (timeStart<=0)
-            {                              
-                timeStart = 3;
-                textBox.text = "";
-                Time.timeScale = 1.0f;
-                del = 0;                
-            }          
-        }
+        CountDown();
     }
-    
+
     public void ActivePauseBt()
-    {      
+    {
         myImageComponent = GetComponent<Image>();
         if (Isbool == false)    //정지상태가 아닐때 누르면(정지버튼)
-        {            
+        {
             Time.timeScale = 0;
             Isbool = true;
             StopPanel.SetActive(true);
             myImageComponent.sprite = Stop;
         }
         else if (Isbool == true)    //정지상태일때 누르면(시작버튼)
-        {            
+        {
             StopPanel.SetActive(false);//명암 그냥 때려박은거임 쉿
-           // StartCoroutine("End");
+                                       // StartCoroutine("End");
             del = 1;  //카운트다운 시작          
-            Time.timeScale = 1.0f;
+            //Time.timeScale = 1.0f;
             Isbool = false;
             timeStart = 3;
             myImageComponent.sprite = Start;    //이미지 바뀌는거
+        }
+    }
+
+    void CountDown()
+    {
+        if (del == 1)
+        {
+            //timeStart -= Time.deltaTime;    //시간 내려가는거
+            timeStart -= Time.unscaledDeltaTime;
+            
+            textBox.text = Mathf.Round(timeStart).ToString();
+
+            if (timeStart <= 0)
+            {
+                timeStart = 3;
+                textBox.text = "";
+                Time.timeScale = 1.0f;
+                del = 0;
+            }
         }
     }
 }
